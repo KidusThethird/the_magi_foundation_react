922724f7842105b57f4bfa34bfb089d8e06a3d44
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-24 22:25:02 +0300

    about section added

---
+++ src/components/about.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='grid grid-cols-2 p-14 text-gray-700'>
+
+<div>
+  <div className='flex text-5xl font-semibold space-x-2'>
+    <h1 className=''>Our</h1> <h1 className='text-sky-500 underline'>Story</h1>
+    </div>
+
+    <h1 className='text-4xl font-semibold py-6'>About the Organization</h1>
+    <p className=''>The Magi Foundation is a local, charitable, non-for profit, non-governmental organization. Its inception was from Australian couple of Ethiopian origin whose true Passion for Supporting Children, brought two Ethiopians to found it and register in Ethiopia in April 2022.</p>
+
+   <div className='flex space-x-4 pt-5'>
+    <button className='border-2 border-sky-700 text-sky-700 rounded-full px-3 py-1'>Read More</button>
+    <button className='text-white bg-sky-700 rounded-full px-3 py-2 '>Check out Events</button>
+    </div> 
+</div>
+
+
+<div className='my-auto p-14'>
+<h1 className='text-xl font-bold '>
+The name “Magi” is a Latin term reflecting on our desire to bring the light of hope to orphans, vulnerable children, and their guardians. We work with communities to support orphans and vulnerable children to reach their full potential as productive members of society.
+</h1>
+</div>
+
+    </div>
+  )
+}
+
+export default About;
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from '../components/hero'
+import About from '../components/about'
 
 
  const home = () => {
   return (
+    <>
     <Hero />
+    <About />
+    </>
+
   )
 }
 
