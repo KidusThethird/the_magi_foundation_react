8aa53c1928b2ed4d7872806517bb21fbc61480f6
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-08-24 22:58:30 +0300

    blog file added to blog js util

---
+++ src/utils/blogs.js
@@ -0,0 +1,49 @@
+import BlogImage1 from '../assets/images/blog/blog1.jpg';
+import BlogImage2 from '../assets/images/blog/blog2.jpg';
+import BlogImage3 from '../assets/images/blog/blog3.jpg';
+import BlogImage4 from '../assets/images/blog/blog4.jpg';
+import BlogImage5 from '../assets/images/blog/blog5.jpg';
+
+
+export const BlogList = [
+    {
+     topic: "THE MAGI children support through their family",
+     genre : "Children Support",
+     highlight: "Many households in Ethiopia have faced hardships as a result of the conflict and the economic crisis. THE MAGI starts its first...",
+     fullText: "Many households in Ethiopia have faced hardships as a result of the conflict and the economic crisis. THE MAGI starts its first work by helping displaced children in Debre Birhan city as a matter of urgency then followed by children school support in Adama city we are so much blessed with what we achieved.   \n  Currently THE MAGI family realised that so many families in Addis Ababa city especially those who are very poor are challenged for survival. We have many children in our waiting list all of them from very poor family. To mention some of their cases, some parents have no food to feed their children and some children have health problems; some parents not have good health to help their kids, some care to their grand children but no strength and poor. So THE MAGI worked well on those family cases and planed to reach out them through support for food provision, facilitate family health insurance access and school support at least two children per family. To accomplish what THE MAGA planed above it cost as 150$ per/child for one year that means the support will be given every month. The mentioned cost above is a lump sum including admin cost too.",
+     image: BlogImage1,
+     displayOnHomePage: true,
+    },
+    {
+        topic: "Meet 8 and 10 year old and brother and sister",
+        genre : "Children Support",
+        highlight: "Meet 8 and 10 year old and brother and sister, Sumeya Abdulkerim and Rahamat Abdulkerim. They are students...",
+        fullText: "Meet 8 and 10 year old and brother and sister, Sumeya Abdulkerim and Rahamat Abdulkerim. They are students and live in the pictured room with the deteriorating ceiling with their father Abdulkerim. Your probably wondering where is the mom in this picture, their mom separated from their father. The father, Abdulkerim suffers from mental illness and can't properly provide for the children. Even so, he stands beside Sumeya and Rahamat, eyes filled with hope for tomorrow. This is the story of two stars motherless and with a mentally ill father, the Magi aims to be the hope for such circumstances.\n\nWe want to create a sustainable living condition so children like Sumeya and Rahamat can not only be filled with hope but with hope enough to one day also impact other lives. Currently the Magi helps provide basic day to day food expenses. The end goal of the Magi is to be able to get these stars in a safe and caring environment where they can pursue their education.\n\n Pursue their education without having to worry about basic necessities, the Magi wants to be that safe haven. The basic necessities that a sustauble living condtion includes clothing, shelter and food, considered luxariouse items to the children the Magi focuses on. Anyone who has read this far can partake whether that be in a one time donation or going as far as sponsoring a child. For more information on this story our contact details are entitled in the contact secetion of this website.",
+        image: BlogImage2,
+        displayOnHomePage: false,
+       },
+       {
+        topic: "School material assistance",
+        genre : "Educational support",
+        highlight: "Currently, quite significant number of families are unable to enroll their school age children in Ethiopia, due to luck of peace...",
+        fullText: "Currently, quite significant number of families are unable to enroll their school age children in Ethiopia, due to luck of peace and stability, and the resulting effect of low or no income.\n\n        In order to address this worsening social issue, The Magi Charity has been providing basic educational materials such as school Uniform and shoes, school bags, exercise books, pens and other essentials to limited number of children referred to us.\n\n        As the scope of the problem is too big and due to resource constraints, we could not have reached as many children as we would like to.\n        We therefore seek your unreserved support by standing with us under our initiative- “Let us be parent to a student”.\n\n   We thank you in advance for your generosity.",
+        image: BlogImage3,
+        displayOnHomePage: true,
+       },
+       {
+        topic: "Loving the unloved",
+        genre : "Street children",
+        highlight: "Street children are boys and girls for whom streets have become home. While some are orphans, many have families but grew up...",
+        fullText: "New hope for Ethiopian street children    \n    Street children are boys and girls for whom streets have become home.\nWhile some are orphans, many have families but grew up in slum-like settlements in extreme poverty.\n Struggling to pay for necessary expenses, parents sometimes send their children to the streets to beg to contribute to the family income.\n        Join The Magi and let us teach them the importance of family life and offer them unconditional parental love.",
+        image: BlogImage4,
+        displayOnHomePage: true,
+       },
+       {
+        topic: "Children of Brave Woman from the Streets of Ethiopia",
+        genre : "Life support",
+        highlight: "Meet brother and sister Huziefa Abdulehadid and Enib Abdulehadid, children of the brave woman Jemil, from the...",
+        fullText: "Meet brother and sister Huziefa Abdulehadid and Enib Abdulehadid, children of the brave woman Jemil, from the streets of Ethiopia. Huziefa is in fourth grade while Enib is in kindergarten. The fort behind them hanging by a couple of large sticks is the place they have been calling home for the past two years. The father of these stars is half paralyzed, suffers from high blood pressure and is also a diabetic.\n        While some are orphans, many have families but grew up in slum-like settlements in extreme poverty.\n        Their mother Jemil provides for them by collecting and selling bottles and plastic bags, and sometimes if she's lucky is able to make money by washing other peoples dirty clothes. Even through the struggle of trying to survive this hard life, Jamil is putting up a brave fight for her children. She is the definition of a true mother a warrior at that.\n        Even through these hard circumstances Jemil is paying the price to put her children through school in hopes of one day seeing them escape the hardships they are currently facing. Imagine how far these children with so much hope could go if they had some support. Imagine the rest this hard working warrior of a mother will feel when her children are being properly educated. The Magi aims to support and address the lives of children like Huziefa and Enib.",
+        image: BlogImage5,
+        displayOnHomePage: false,
+       },
+]
