0678a0fb6af3627ceffe85d6ac973aa3c97575ae
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-09-03 13:30:48 +0300

    home-about section responsivness added

--- src/components/homePageComponents/about.jsx
+++ src/components/homePageComponents/about.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='grid grid-cols-2 p-14 text-gray-700'>
+    <div className='slg:grid grid-cols-2 p-14 text-gray-700 space-x-4'>
 
 <div>
-  <div className='flex text-5xl font-semibold space-x-2'>
+  <div className='flex text-3xl sm:text-5xl font-semibold space-x-2'>
     <h1 className=''>Our</h1> <h1 className='text-sky-500 underline'>Story</h1>
     </div>
 
-    <h1 className='text-4xl font-semibold py-6'>About the Organization</h1>
+    <h1 className='text-2xl sm:text-4xl font-semibold py-6'>About the Organization</h1>
     <p className=''>The Magi Foundation is a local, charitable, non-for profit, non-governmental organization. Its inception was from Australian couple of Ethiopian origin whose true Passion for Supporting Children, brought two Ethiopians to found it and register in Ethiopia in April 2022.</p>
 
    <div className='flex space-x-4 pt-5'>
@@ -19,8 +19,8 @@ const About = () => {
 </div>
 
 
-<div className='my-auto p-14'>
-<h1 className='text-xl font-bold '>
+<div className='my-auto pt-5'>
+<h1 className='text-lg sm:text-xl font-bold '>
 The name “Magi” is a Latin term reflecting on our desire to bring the light of hope to orphans, vulnerable children, and their guardians. We work with communities to support orphans and vulnerable children to reach their full potential as productive members of society.
 </h1>
 </div>
