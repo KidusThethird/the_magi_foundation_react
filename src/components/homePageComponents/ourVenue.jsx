75f6aa86a37be975bb9c42dad080f94b5f74fe80
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-09-03 16:36:57 +0300

    home our venue section responsiveness added

--- src/components/homePageComponents/ourVenue.jsx
+++ src/components/homePageComponents/ourVenue.jsx
@@ -5,13 +5,13 @@ import {MapPin , Envelope, Phone   } from "@phosphor-icons/react";
 const OurVenue = () => {
   return (
     <div>
-        <div className='flex text-5xl font-semibold space-x-3 p-5'>
+        <div className='flex text-3xl smd:text-4xl md:text-5xl font-semibold space-x-3 p-5'>
         <h1 className='text-gray-700 '>Our</h1>
         <h1 className='text-sky-700 underline'>Venue</h1>
         </div>
 
 
-        <div className='grid grid-cols-2 p-10 gap-x-5 '>
+        <div className='flex slg:grid flex-col-reverse grid-cols-2 p-10 gap-x-5 '>
 
         <div className="w-full  mx-auto rounded-xl overflow-hidden border-2 border-sky-700 border-opacity-30">
   <div className="relative h-0 " style={{ paddingBottom: '66.66%' }}>
@@ -21,9 +21,9 @@ const OurVenue = () => {
 
         <div className='h-full '>
             <div className='flex w-full  bg-sky-700 rounded-t-3xl'>
-            <h1 className=' text-white text-4xl font-semibold py-7 j mx-auto'>The Magi Foundation</h1>
+            <h1 className=' text-white text-2xl smd:text-3xl md:text-4xl font-semibold py-7  mx-auto'>The Magi Foundation</h1>
             </div>
-            <div className='text-2xl font-semibold  text-gray-700 justify-evenly flex flex-col h-3/4  shadow-2xl shadow-sky-700 '>
+            <div className='text-base smd:text-xl md:text-2xl font-semibold  text-gray-700 justify-evenly flex flex-col h-3/4  shadow-2xl shadow-sky-700 '>
             <div className='flex justify-center gap-x-2'>
                 <MapPin className='my-auto'/>
                <h1 > Ras Desta Damtew St, Addis Ababa</h1>
